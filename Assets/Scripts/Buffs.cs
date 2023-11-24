63b9fa49af4e63a0400fcba386a17130d0216c99
Afectto <138150311+Afectto@users.noreply.github.com>
2023-11-24 20:21:29 +0700

    AddBuffByName

--- Assets/Scripts/Buffs.cs
+++ Assets/Scripts/Buffs.cs
@@ -5,9 +5,26 @@ public class Buffs : MonoBehaviour
 {
     public List<GameObject> prefabs;
     public BuffFactory BuffFactory;
+
+    private List<GameObject> buffs;
+
     void Start()
     {
-        BuffFactory.CreateBuffGroup(prefabs,transform);
+        buffs = new List<GameObject>();
+        AddBuffByName("BuffDex");
+        AddBuffByName("BuffDex");
+        AddBuffByName("BuffInt");
+        AddBuffByName("BuffStr");
+        BuffFactory.CreateBuffGroup(buffs, transform);
+    }
+
+    private void AddBuffByName(string name)
+    {
+        var buff = prefabs.Find(buffInfo => buffInfo.name == name);
+        if (buff is not null)
+        {
+            buffs.Add(buff);
+        }
     }
 
 }
